f188633c0f3871ebaf42520bbcea7ca40915e7c0
Andre Martins <martins.lopes@pucpr.edu.br>
2022-04-07 20:47:51 -0300

    criação projeto base

---
+++ system/src/main/java/com/pucpr/system/ErpFarmaciaApplication.java
@@ -0,0 +1,13 @@
+package com.pucpr.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpFarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpFarmaciaApplication.class, args);
+	}
+
+}
