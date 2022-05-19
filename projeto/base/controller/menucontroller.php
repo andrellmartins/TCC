bffbbd941e383ff6f161772976d3fda73267d8db
Andre Martins <martins.lopes@pucpr.edu.br>
2022-05-19 19:15:07 -0300

    correção na exibição da mensagem após login

--- projeto/base/controller/menucontroller.php
+++ projeto/base/controller/menucontroller.php
@@ -5,10 +5,6 @@ class menucontroller extends controller{
     public function menu(){
             $menu = new menu;
             $content = navbar::render(true).$menu->render(true);
-            if($_SESSION['usuarioLogado']){
-                mensagensPadroes::msgBemVindo();
-                $_SESSION['usuarioLogado'] = false;
-            }
             $interface = new interfacePadrao;
             $interface->setTitulo('Menu');
             $interface->setContent($content);
